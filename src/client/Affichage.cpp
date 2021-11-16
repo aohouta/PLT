962fe08118d22ca0d9a0a35709cfdabd7cef7f3e
papa.pc@hotmail.fr <papa.pc@hotmail.fr>
2021-11-16 16:37:17 +0100

    Tesst d'affichage

--- src/client/Affichage.cpp
+++ src/client/Affichage.cpp
@@ -1,9 +1,15 @@
 
-
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 void afficheState(){
     sf::RenderWindow window(sf::VideoMode(800, 600), "UAGT", sf::Style::Default);
+    sf::Texture texture;
+    if (!texture.loadFromFile("res/archer.png")){
+        std::cout << "\nOOPSIE DOOPSIE\n";
+    }
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
     window.setMouseCursorVisible(false);
     while(window.isOpen()){
         sf::Event event;
@@ -15,5 +21,10 @@ void afficheState(){
         window.clear(sf::Color::Black);
         sf::CircleShape shape(50.f);
         shape.setFillColor(sf::Color(100, 250, 50));
+        shape.setPosition(10.f, 50.f);
+        sprite.setPosition(20, 30);
+        window.draw(shape);
+        window.draw(sprite);
+        window.display();
     }
 }
