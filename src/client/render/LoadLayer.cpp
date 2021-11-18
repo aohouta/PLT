e23a3d22cebc81ce919fd2a0da5b512323913b3e
BastGuil <91589390+BastGuil@users.noreply.github.com>
2021-11-18 10:51:49 +0100

    changement  dia + Cpp render

--- src/client/render/LoadLayer.cpp
+++ src/client/render/LoadLayer.cpp
@@ -9,40 +9,41 @@ namespace render{
 
 bool loadTextures (state::State& state, sf::Texture& Tile_texture, sf::Vector2u tile_Size, unsigned int width, unsigned int height)
 {
-    sf::Texture texture = Tile_texture;
+    // load the tileset texture
+    if (!m_tileset.loadFromFile(tileset))
+        return false;
+    
+    // resize the vertex array to fit the level size
+    m_vertices.setPrimitiveType(sf::Quads);
+    m_vertices.resize(width * height * 4);
 
-    // on redimensionne le tableau de vertex pour qu'il puisse contenir tout le niveau
-    quads.setPrimitiveType(sf::Quads);
-    quads.resize(width * height * 4);
-
-    // on remplit le tableau de vertex, avec un quad par tuile
-    for (unsigned int i = 0; i < state.getMap().size(); ++i)
-    {
-        for (unsigned int j = 0; j < state.getMap()[0].size(); j++)
+    // populate the vertex array, with one quad per tile
+    for (unsigned int i = 0; i < width; ++i)
+        for (unsigned int j = 0; j < height; ++j)
         {
             // get the current tile number
-            int tileNumber = state.getMap()[i][j]->getTileCode();
-            // on en déduit sa position dans la texture du tileset
-            int tu = tileNumber % (texture.getSize().x / tile_Size.x);
-            int tv = tileNumber / (texture.getSize().x / tile_Size.x);
+            int tileNumber = tiles[i + j * width];
 
-            // on récupère un pointeur vers le quad à définir dans le tableau de vertex
-            sf::Vertex *quad = &quads[(i + j * width) * 4];
+            // find its position in the tileset texture
+            int tu = tileNumber % (m_tileset.getSize().x / tile_Size.x);
+            int tv = tileNumber / (m_tileset.getSize().x / tile_Size.x);
+
+            // get a pointer to the current tile's quad
+            sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
 
             // define its 4 corners
-            quad[0].position = sf::Vector2f(j * tile_Size.x, i * tile_Size.y);
-            quad[1].position = sf::Vector2f((j + 1) * tile_Size.x, i * tile_Size.y);
-            quad[2].position = sf::Vector2f((j + 1) * tile_Size.x, (i + 1) * tile_Size.y);
-            quad[3].position = sf::Vector2f(j * tile_Size.x, (i + 1) * tile_Size.y);
+            quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+            quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+            quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+            quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
 
             // define its 4 texture coordinates
-            quad[0].texCoords = sf::Vector2f(tu * tile_Size.x, tv * tile_Size.y);
-            quad[1].texCoords = sf::Vector2f((tu + 1) * tile_Size.x, tv * tile_Size.y);
-            quad[2].texCoords = sf::Vector2f((tu + 1) * tile_Size.x, (tv + 1) * tile_Size.y);
-            quad[3].texCoords = sf::Vector2f(tu * tile_Size.x, (tv + 1) * tile_Size.y);
+            quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+            quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+            quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+            quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+            }
 
-        }
-    }
     return true;
 }
       
@@ -50,7 +51,7 @@ bool loadTextures (state::State& state, sf::Texture& Tile_texture, sf::Vector2u 
 void draw (sf::RenderTarget& target, sf::RenderStates states){
     {
         // apply the transform
-        states.transform *= getTransform();
+        states.transform *= Transform();
 
         // apply the tileset texture
         states.texture = &texture;
