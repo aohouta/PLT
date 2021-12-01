d869fe0bd80dc430d048c549c97e7e51372ad491
papa.pc@hotmail.fr <papa.pc@hotmail.fr>
2021-12-01 17:20:23 +0100

    MoveCommand

---
+++ src/shared/engine/MoveCommand.cpp
@@ -0,0 +1,34 @@
+#include "MoveCommand.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+namespace engine{
+    MoveCommand::MoveCommand(state::Cell TargetCell){
+        this->TargetCell = TargetCell;
+        perso = state::Etat::getActivePlayer();
+    }
+    int MoveCommand::Execute(state::Etat& state){
+        int occupation = TargetCell.isOccupied();
+        if(occupation = 0){
+            if(pathfinding()){
+                perso.setPosition(TargetCell.getPosition());
+            }
+            else {
+                cout << "Too far\n";
+            }
+        }
+        else {
+            cout << "You can't do that\n";
+        }
+    }
+    bool MoveCommand::Pathfinding(){
+        int posCell[] = TargetCell.getPosition();
+        int posPerso[] = Perso.getPosition();
+        int dist = abs(posCell[0]-posPerso[0])+abs(posCell[1]-posPerso[1]);
+        return dist>Perso.getMOB();
+    }
+}
