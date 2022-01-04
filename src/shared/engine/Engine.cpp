515ecb081259f6bfd74fdc79fd9827ce8158c74e
papa.pc@hotmail.fr <papa.pc@hotmail.fr>
2022-01-04 12:06:09 +0100

    Probleme on thread

--- src/shared/engine/Engine.cpp
+++ src/shared/engine/Engine.cpp
@@ -6,23 +6,26 @@ using namespace std;
 
 namespace engine{
     void Engine::Start(state::State& state){
-        inc = thread(Increment, state);
+        inc = thread(&Engine::Increment, this, state);
     }
     void Engine::Increment(state::State& state){
         while(1){
             if(state.activePlayer == nullptr){
                 int vitMax = 100;
                 for(auto& perso : state.getPersonnages()){
-                    perso.VitessePos += perso.getVitesse();
-                    if(perso.VitessePos > vitMax){
-                        vitMax = perso.VitessePos;
+                    perso->VitessePos += perso->getVIT();
+                    if(perso->VitessePos > vitMax){
+                        vitMax = perso->VitessePos;
                         auto active = perso;
+                        state.activePlayer = active;
                     }
                 }
-                state.activePlayer = shared_ptr<active>;
             }
             sleep(1);
         }
+    }
+    void Engine::EndTurn(state::State& state){
+        state.activePlayer = nullptr;
     }
     void Engine::Stop(){
         inc.join();
