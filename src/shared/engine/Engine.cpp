18cc51896117f741b982c10e16dcc9916945310d
williamduval45@gmail.com <williamduval45@gmail.com>
2022-01-20 00:56:49 +0100

    engine update to merge

--- src/shared/engine/Engine.cpp
+++ src/shared/engine/Engine.cpp
@@ -1,14 +1,53 @@
 #include "Engine.h"
-#include <stdio.h>
+#include <thread>
+#include <unistd.h>
 #include <iostream>
-
-
 
 using namespace std;
 
-
-
 namespace engine{
+    Engine::Engine(state::State& state){
+        this->state = &state;
+    }
+    void Engine::Start(){
+        inc = thread(&Engine::Increment, this);
+    }
+    void Engine::Increment(){
+        while(!state->gameOver){
+            stateMute.lock();
+            if(state->activePlayer == nullptr){
+                int vitMax = 100;
+                for(auto& perso : state->getPersonnages()){
+                    perso->VitessePos += perso->getVIT();
+                    cout << "name = " << perso->getPType() << "Vitesse actuelle =" << perso->VitessePos << endl ;
+                    if(perso->VitessePos >= vitMax){
+                        vitMax = perso->VitessePos;
+                        state->activePlayer = perso;
+                    }
+                }
+                cout << endl;
+            }
+            if(!(state->activePlayer == nullptr)) {
+                state->activePlayer->VitessePos = 0;
+            }
+            stateMute.unlock();
+            sleep(1);
+        }
+    }
+    void Engine::EndTurn(){
+        state->activePlayer = nullptr;
+        int vitMax = 100;
+        for(auto& perso : state->getPersonnages()){
+            cout << "name = " << perso->getPType() << "Vitesse actuelle =" << perso->VitessePos << endl;
+            if(perso->VitessePos >= vitMax){
+                vitMax = perso->VitessePos;
+                state->activePlayer = perso;
+            }
+        }
+    }
+    void Engine::Stop(){
+        inc.join();
+    }
 
     void Engine::UpdateEffet(shared_ptr<state::Personnage> target){
 
@@ -29,5 +68,6 @@ namespace engine{
 
         target->setListeEffet(newListeEffet);
     }
+
 
 }
